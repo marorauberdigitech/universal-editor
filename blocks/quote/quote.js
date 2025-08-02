4cc733599f985b45b02698938e72602a03f79761
marora <marora@uberdigitech.com>
2025-08-02 20:14:56 +0530

    added

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,7 +1,30 @@
 export default function decorate(block) {
-  const [quoteWrapper] = block.children;
+  const [quoteWrapper, authorWrapper, imageWrapper] = block.children;
+
+  const quoteText = quoteWrapper?.textContent.trim();
+  const authorText = authorWrapper?.textContent.trim();
+  const imageEl = imageWrapper?.querySelector('img');
 
   const blockquote = document.createElement('blockquote');
-  blockquote.textContent = quoteWrapper.textContent.trim();
-  quoteWrapper.replaceChildren(blockquote);
+  blockquote.textContent = quoteText;
+
+  const authorDiv = document.createElement('div');
+  authorDiv.textContent = authorText;
+
+  const quoteContentWrapper = document.createElement('div');
+  quoteContentWrapper.appendChild(blockquote);
+  quoteContentWrapper.appendChild(authorDiv);
+
+  const finalWrapper = document.createElement('div');
+  finalWrapper.classList.add('quote-inner');
+
+  if (imageEl) {
+    const imgContainer = document.createElement('div');
+    imgContainer.classList.add('quote-image');
+    imgContainer.appendChild(imageEl);
+    finalWrapper.appendChild(imgContainer);
+  }
+
+  finalWrapper.appendChild(quoteContentWrapper);
+  block.replaceChildren(finalWrapper);
 }
